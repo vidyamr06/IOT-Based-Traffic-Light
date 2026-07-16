0d2a82eb129f04d50980e767730da33f6092eee1
Vidyashree MR <vidyamr8431@gmail.com>
2026-07-16 08:51:38 -0500

    Add files via upload

---
+++ TRAFFIC_LIGHT.ino
@@ -0,0 +1,28 @@
+const int led1=8;
+const int led2=4;
+const int led3=2;
+void setup() {
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+}
+void loop() {
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,LOW);
+  delay(1000);
+  digitalWrite(led1,LOW);
+  digitalWrite(led2,HIGH);
+  digitalWrite(led3,LOW);
+  delay(1000);
+  digitalWrite(led1,LOW);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,HIGH);
+  delay(1000);
+  digitalWrite(led1,LOW);
+  digitalWrite(led2,HIGH);
+  digitalWrite(led3,LOW);
+  delay(1000);
+  
+
+}
